5bc78a7f492ca1e106a7e45613a61df50b487b64
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-09-22 08:19:41 +0100

    Add files via upload

--- num.py
+++ num.py
@@ -1,8 +1,21 @@
+def difs(arra1):
+    difint=[]
+    for n in range(0,len(nps)-1):
+        difint=difint+[nps[n+1]-nps[n]]
+    return difint
+def sums(arr1):
+    l=len(arr1)
+    s:float=0
+    for n in range(l):
+        s=s+arr1[1]
+    return l,s
+
 print("\033c\033[43;30m\n")
 nps=[]
 nps=[1,2,3,4,5,6,7,8,9]
-difint=[]
-for n in range(0,len(nps)-1):
-    difint=difint+[nps[n+1]-nps[n]]
+
 print(nps)
-print(" "+str(difint))
+d=difs(nps)
+print(" "+str(d))
+l,n=sums(d)
+print(n/l)
